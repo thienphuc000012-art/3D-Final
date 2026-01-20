772dfbc0b89b0285c1c25535220c0b05baf260cc
Son <tqs4work@gmail.com>
2026-01-20 15:22:32 +0700

    feat: merge

--- Assets/Working/Database/Scripts/UpdateCanvas.cs
+++ Assets/Working/Database/Scripts/UpdateCanvas.cs
@@ -5,6 +5,8 @@ public class UpdateCanvas : MonoBehaviour
 {
     GameObject canvas;
     GameObject InputIngameNamePanel;
+    GameObject MenuPanel;
+    public GameObject PlayerCam;
 
     void Awake()
     {
@@ -15,10 +17,12 @@ public class UpdateCanvas : MonoBehaviour
     {
         canvas = GameObject.Find("Canvas").gameObject;
         InputIngameNamePanel = canvas.transform.Find("IngameNameInputPanel").gameObject;
+        MenuPanel = canvas.transform.Find("MenuPanel").gameObject;
         Debug.Log("PlayerRuntime.Instance.name: " + PlayerRuntime.Instance.Player.Name);
         if (PlayerRuntime.Instance.Player.Name != "NewPlayer")
         {
             InputIngameNamePanel.SetActive(false);
+            PlayerCam.SetActive(true);
         }
         else
         {
@@ -38,5 +42,12 @@ public class UpdateCanvas : MonoBehaviour
         InputIngameNamePanel.SetActive(false);
         _ = PlayerRuntime.Instance.SavePlayer();
         Debug.Log("name after input: " + PlayerRuntime.Instance.Player.Name);
+        MenuPanel.SetActive(true);
+    }
+
+    public void PlayButtonClick()
+    {
+        MenuPanel.SetActive(false);
+        PlayerCam.SetActive(true);
     }
 }
