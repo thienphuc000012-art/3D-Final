f9fd39d4fa189290380ed68903df04cf7d282bcc
Son <tqs4work@gmail.com>
2026-03-10 19:49:53 +0700

    1

--- Assets/Working/Database/Scripts/UpdateCanvas.cs
+++ Assets/Working/Database/Scripts/UpdateCanvas.cs
@@ -5,8 +5,24 @@ public class UpdateCanvas : MonoBehaviour
 {   
     [SerializeField] private TextMeshProUGUI Ammotext;
     [SerializeField] GameObject player;
+
+    public GunData gunData;
     int currentAmmo;
     int maxAmmo;
+
+    float damage, fireCooldown, reloadTime, bulletSpeed;
+    public int magazineSize;
+
+    int level = 1;
+
+    [Header("Upgrade")]
+    [SerializeField] TextMeshProUGUI curDamageText;
+    [SerializeField] TextMeshProUGUI curFireRateText;
+    [SerializeField] TextMeshProUGUI curMagazineText;
+    [SerializeField] TextMeshProUGUI nextDamageText;
+    [SerializeField] TextMeshProUGUI nextFireRateText;
+    [SerializeField] TextMeshProUGUI nextMagazineText;
+
     void Awake()
     {
         player = GameObject.Find("Player_T");
@@ -19,6 +35,7 @@ public class UpdateCanvas : MonoBehaviour
 
     void Update()
     {
+        UpdateGunStats();
         if (player != null)
         {
             currentAmmo = player.transform.Find("GunLogic").GetComponent<Gun>().currentAmmo;
@@ -29,7 +46,41 @@ public class UpdateCanvas : MonoBehaviour
 
     }
 
-    //_ = PlayerRuntime.Instance.SavePlayer();
-    
-    
-}
+    void UpdateGunStats()
+    {
+        damage = gunData.damage;
+        fireCooldown = gunData.fireRate;
+        reloadTime = gunData.reloadTime;
+        bulletSpeed = gunData.bulletSpeed;
+        magazineSize = gunData.magazineSize;
+        currentAmmo = magazineSize;
+    }
+    void CurGunStatsText()
+    {
+        curDamageText.text = damage.ToString("F1");
+        curFireRateText.text = fireCooldown.ToString("F2") + "s";
+        curMagazineText.text = magazineSize.ToString();
+    }
+    void NextGunStatsText()
+    {
+        int lv = level - 1;
+
+        damage = gunData.damage * (1f + 0.5f * lv);
+        fireCooldown = gunData.fireRate * (1f - 0.15f * lv);
+        bulletSpeed = gunData.bulletSpeed * (1f + 0.1f * lv);
+        magazineSize = gunData.magazineSize + lv * 5;
+
+        currentAmmo = magazineSize;
+    }
+
+        //_ = PlayerRuntime.Instance.SavePlayer();
+
+
+
+    //damage = gunData.damage;
+    //    fireCooldown = gunData.fireRate;
+    //    reloadTime = gunData.reloadTime;
+    //    bulletSpeed = gunData.bulletSpeed;
+    //    magazineSize = gunData.magazineSize;
+    //    currentAmmo = magazineSize;
+    }
